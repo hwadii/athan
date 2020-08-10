7b77e6e2e7480f809af6a2df5280b185280e6f25
Wadii Hajji <wadii.hajji@etu.univ-amu.fr>
2020-08-10 15:39:32 +0100

    Nicer error msgs

--- lib/parser.rb
+++ lib/parser.rb
@@ -9,15 +9,15 @@ class Parser
     OptionParser.new do |parser|
       parser.on('-t', '--three', 'Get the three next days of prayer timings') do
         @options[:three] = true
-        raise ArgumentError.new '-t (three) and -f (fetch) are mutually exclusive. Use one or the other.' if @options.key?(:fetch)
+        abort '-t (three) and -f (fetch) are mutually exclusive. Use one or the other.' if @options.key?(:fetch)
       end
       parser.on('-J', '--json', 'Get output as a json') do
         @options[:json] = true
       end
       parser.on('-fDATE', '--fetch DATE', 'Fetch prayer timings for a given date') do |date|
         date = Date.parse(date) rescue nil
-        raise Date::Error.new 'Date is invalid. Give a valid format, e.g. YYYY-MM-DD.' if date.nil?
-        raise ArgumentError.new '-t (three) and -f (fetch) are mutually exclusive. Use one or the other.' if @options.key?(:three)
+        abort '-t (three) and -f (fetch) are mutually exclusive. Use one or the other.' if @options.key?(:three)
+        abort 'Date is invalid. Give a valid format, e.g. YYYY-MM-DD.' if date.nil?
         @options[:fetch] = date
       end
       parser.on('-sSEP', '--sep SEP', 'Specify separator. Default: \'/\'') do |sep|
@@ -38,7 +38,7 @@ class Parser
   def get_location
     sep = @options[:sep] || '/'
     chunks = ARGV.first&.split(sep, 2)
-    exit 1 if chunks&.size < 2
+    abort 'Incorrect formatting for city and country. Expected \'City/Country\'.' if chunks&.size < 2
     chunks
   end
 end
