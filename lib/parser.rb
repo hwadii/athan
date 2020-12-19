fa28c54f0282d7e7ff67863cef9933472d5c3897
Wadii Hajji <wadii@cardiologs.com>
2020-12-19 23:48:51 +0100

    Squashed commit of the following:
    commit 4ccf88323150029a541d8730f64c1afcb7373725
    Author: Wadii Hajji <wadii@cardiologs.com>
    Date:   Sat Dec 19 22:49:37 2020 +0100
    
        Implement bitmasks for flags
    
    commit 424bdb3873d2b9563fa760e28e854cfec826661b
    Author: Wadii Hajji <wadii@cardiologs.com>
    Date:   Sun Dec 13 02:54:17 2020 +0100
    
        wip: Working on bitmasks api

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,44 +1,60 @@
 require 'optparse'
 
 class Parser
+  attr_reader :flags, :options
+
+  FLAGS = {
+    Three: 0b000001,
+    Json: 0b000010,
+    Date: 0b000100,
+    Sep: 0b001000,
+    More: 0b010000,
+    Next: 0b100000,
+  }.freeze
+
   def initialize
-    @options = {}
+    @flags = 0
+    @options = {
+      sep: nil,
+      date: nil,
+    }.compact
   end
 
   def parse
     OptionParser.new do |parser|
+      parser.banner = 'Usage: athan [options] [location]'
       parser.on('-t', '--three', 'Get the three next days of prayer timings') do
-        @options[:three] = true
-        abort '-t (three) and -f (fetch) are mutually exclusive. Use one or the other.' if @options.key?(:fetch)
-      end
-      parser.on('-J', '--json', 'Get output as a json') do
-        @options[:json] = true
+        @flags |= FLAGS[:Three]
+        abort '-t (three) and -f (fetch) are mutually exclusive. Use one or the other.' if @flags & FLAGS[:Date] == 1
       end
       parser.on('-fDATE', '--fetch DATE', 'Fetch prayer timings for a given date') do |date|
+        @flags |= FLAGS[:Date]
+        abort '-t (three) and -f (fetch) are mutually exclusive. Use one or the other.' if @flags & FLAGS[:Three] == 1
         date = Date.parse(date) rescue nil
-        abort '-t (three) and -f (fetch) are mutually exclusive. Use one or the other.' if @options.key?(:three)
         abort 'Date is invalid. Give a valid format, e.g. YYYY-MM-DD.' if date.nil?
-        @options[:fetch] = date
+        @options[:date] = date
+      end
+      parser.on('-J', '--json', 'Get output as a json') do
+        @flags |= FLAGS[:Json]
       end
       parser.on('-sSEP', '--sep SEP', 'Specify separator. Default: \'/\'') do |sep|
+        @flags |= FLAGS[:Sep]
         @options[:sep] = sep
       end
       parser.on('-m', '--more', 'Display all the possible timings. Default: 5 timings.') do
-        @options[:more] = true
+        @flags |= FLAGS[:More]
       end
       parser.on('-n', '--next', 'Show next prayer timing of the day.') do
-        @options[:next] = true
+        @flags |= FLAGS[:Next]
       end
     end.parse!
-    city, country = get_location
-    @options[:city] = city
-    @options[:country] = country
-    @options
+
+    location
   end
 
   private
 
-  def get_location
+  def location
     sep = @options[:sep] || '/'
     chunks = ARGV.first&.split(sep, 2)
     abort "Incorrect formatting for city and country. Expected 'City#{sep}Country'." if chunks&.size < 2
